3dcd4206e4cd6cc86f9515904a06116208bf81de
puge <mail@puge.org>
2020-07-21 10:54:40 +0800

    save

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Mon Jul 20 2020 22:39:56 GMT+0800 (GMT+08:00)
+// Tue Jul 21 2020 10:18:52 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},};
 /* 方法合集 */
 var _owo = {
--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 
+function postMsg() {
+	if (document.form.comment.value==null || document.form.comment.value=="") {
+		alert("请填写留言。");
+		return;
+	} else {
+		document.form.submit();
+	}
+}
